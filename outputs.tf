5683d56907df71d264d0331609bff71280d68ba8
EC2 Default User <ec2-user@ip-172-31-31-215.ec2.internal>
2021-02-12 14:40:23 +0000

    Initial commit

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+
+###This chunk of template can also be put inside outputs.tf for better segregation 
+output "Jenkins-Main-Node-Public-IP" {
+  value = aws_instance.jenkins-master.public_ip
+}
+
+output "Jenkins-Worker-Public-IPs" {
+  value = {
+    for instance in aws_instance.jenkins-worker-oregon :
+    instance.id => instance.public_ip
+  }
+}
